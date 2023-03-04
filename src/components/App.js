ab5a330dce4e9aedb4d1b2c15ebb635d0b1e4415
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-03-04 13:44:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import '../styles/App.css';
-import { useEffect , useState } from 'react';
-
-/**
- * @task : fetch data from the api, and show to frontend.  
- * @url : https://jsonplaceholder.typicode.com/users
- * @output : show the names of users to frontend like below. 
- * 0. Leanne Graham
-1. Ervin Howell
-2. Clementine Bauch
-3. Patricia Lebsack
-4. Chelsey Dietrich
-5. Mrs. Dennis Schulist
-6. Kurtis Weissnat
- */
 
 function App() {
-
   const [data, setData] = useState([]);
-fetch('https://jsonplaceholder.typicode.com/users')
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
       .then((response) => {
-setData(response);
-}
-  /**
-   * fetch data from api on mount. 
-   */
+        setData(response);
+      });
+  }, []);
 
   return (
-  <div className='App'>
-    { data.map((_data, i) => <p key={_data.id}>{i}.&nbsp;{_data.name}</p>) }
-  </div>
-  ); 
+    <div className='App'>
+      {data.map((_data, i) => (
+        <p key={_data.id}>
+          {i}. {_data.name}
+        </p>
+      ))}
+    </div>
+  );
 }
-/**
- * @task1 : fetch data from this api and show to ui ;
- * 
- */
+
 export default App;
